a773b8194fbf6984cba54bfa5a99813e98b60263
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-06-17 13:34:39 +0100

    criado uma função para corrigir a navbar

---
+++ assets/js/fix.js
@@ -0,0 +1,19 @@
+//correção do menu que estava sendo sobreposto pelo quadro top casa fina na header.
+//cifrão faz a seleção do jquery no js. dentro irá a classe que queremos criar o evento 
+/*iremos criar um evento  o .on fará uma função em que dentro da id #collapse-navbar quando .on for
+clicado para show (mostrar).bscollapse teremos a função que pega a classe banner e muda ela de posição com 
+o transform translate. Quando for clicado para esconder (hide) ele volta para a posição original
+*/ 
+$('#collapse-navbar').on('show.bs.collapse', function(){
+    $('.banner').css('transform', 'translate(-50%, 10%)');
+
+    } 
+    
+)
+
+$('#collapse-navbar').on('hide.bs.collapse', function(){
+    $('.banner').css('transform', 'translate(-50%, -50%)');
+
+    } 
+    
+)
