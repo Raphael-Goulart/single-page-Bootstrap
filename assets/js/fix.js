aaa3f280c9e3c1aba299ec22916d462820496fcc
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-06-17 14:43:01 +0100

    configurando responsividade desktop

--- assets/js/fix.js
+++ assets/js/fix.js
@@ -2,7 +2,8 @@
 //cifrão faz a seleção do jquery no js. dentro irá a classe que queremos criar o evento 
 /*iremos criar um evento  o .on fará uma função em que dentro da id #collapse-navbar quando .on for
 clicado para show (mostrar).bscollapse teremos a função que pega a classe banner e muda ela de posição com 
-o transform translate. Quando for clicado para esconder (hide) ele volta para a posição original
+o transform translate. Quando for clicado para esconder (hide) ele volta para a posição original note que não
+foi preciso criar while.
 */ 
 $('#collapse-navbar').on('show.bs.collapse', function(){
     $('.banner').css('transform', 'translate(-50%, 10%)');
